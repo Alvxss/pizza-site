7641f418b1a61f5b8bcf5f7e0ea20d3331f5c1d8
Alvxss <alvxss20@gmail.com>
2023-10-05 15:15:47 -0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+
+import Landpage from './page/landpage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Landpage />}/>
+      </Routes>
+    </BrowserRouter>    
   </React.StrictMode>
 );
--- src/page/landpage/index.js
+++ src/page/landpage/index.js
@@ -1,1 +1,54 @@
 import './index.scss';
+
+export default function Landpage() {
+  return (
+    <div className='pagina-landpage'>
+      <div className='cabecalho'>
+        <h1>Pizzaria</h1>
+        <img src="" alt="" />
+        <h1>Papa's Louis</h1>
+      </div>
+
+      <div className='secao'>
+        <div className='card'>
+          <div className='foto'>
+            <div>
+              <img src="" alt="" />
+              <div>
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+              </div>
+            </div>
+            <div className='addFoto'>
+              <a href="">ADICIONAR FOTO</a>
+            </div>
+          </div>
+          <div className='detalhesProduto'>
+            <div className='produto'>
+              <div>
+                <h3>Nome do Produto</h3>
+                <input type="text" />
+              </div>
+              <div>
+                <h3>Preço Inicial</h3>
+                <input type="text" />
+              </div>
+              <div>
+                <h3>Quantidade</h3>
+                <input type="text" />
+              </div>
+            </div>
+            <div className='ingredientes'>
+              <h3>ingredientes</h3>
+              <input type="text" />
+            </div>
+            <div className='addProduto'>
+              <a href="">ADICIONAR PRODUTO</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
